38f99af10a487040dad44b69783768afc4c331f7
Ulugbek <39007919+ShadowOutlook@users.noreply.github.com>
2024-11-20 17:21:18 +0700

    Delete Pr№5 directory

--- Pr№5/SQLQuery3.sql
+++
@@ -1,7 +0,0 @@
-USE IAb221_SabirovU;
-
-ALTER TABLE Discipline
-ADD BriefDescripiton VARCHAR(255);
-
-ALTER TABLE Teacher
-ADD PhotoOfTheTeacher VARBINARY(MAX);
--- Pr№5/SQLQuery4.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE Student
-ADD CONSTRAINT chk_DateOfAddmission CHECK (YEAR(DateOfAdmissionToUniversity) > 2000);
